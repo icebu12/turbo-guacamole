a17997639a34cedec030ffb93f3ec81f238d30f7
icebu12 <xidoj-icebu12@hotmail.com>
2026-05-21 21:57:46 +0300

    Update update.js

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 
 const REDIRECT_URL = "https://trgoalsgiris.xyz";
-const BASE_PATTERN = "https://taraftarium1030.xyz/";
+const BASE_PATTERN = "https://taraftarium1071.xyz/";
 const CONFIG_PAGE_PATH = "/channel.html?id=trgoals";
 const MAX_ATTEMPTS = 15;
 const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
